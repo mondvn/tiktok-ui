bf0ea33e262eadd413669c8ad294a17d9a4bdeda
MondVn <trannamanh94@gmail.com>
2022-12-31 22:22:31 +0700

    Cấu hình router/layout cho dự án

---
+++ src/components/Layout/HeaderOnly/index.js
@@ -0,0 +1,17 @@
+import Header from '~/components/Layout/components/Header'
+
+
+function DefaultLayout({ children }) {
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <div className="Content">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DefaultLayout;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from '~/App';
 import reportWebVitals from './reportWebVitals';
+import GlobalStytes from '~/components/GlobalStytes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStytes>
+      <App />
+    </GlobalStytes>
   </React.StrictMode>
 );
 
