aa1db951ace94538e919cafce3bf84fe946cb2f6
MondVn <trannamanh94@gmail.com>
2023-01-08 18:03:26 +0700

    Xây dựng Logic phần Header #3 useDebounce

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -6,10 +6,11 @@ import {
   faSpinner
 } from '@fortawesome/free-solid-svg-icons'
 import classNames from 'classnames/bind'
-import styles from './Search.module.scss'
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
 import { SearchIcon } from '~/components/Icon';
+import { useDebounce } from '~/hooks';
+import styles from './Search.module.scss'
 
 const cx = classNames.bind(styles);
 
@@ -19,16 +20,18 @@ function Search() {
   const [showResult, setShowResult] = useState(true)
   const [loading, setLoading] = useState(false)
 
+  const debounced = useDebounce(searchValue, 500)
+
   const inputRef = useRef()
 
   useEffect(() => {
-    if (!searchValue.trim()) {
+    if (!debounced.trim()) {
       setSearchResult([])
       return
     }
     setLoading(true)
 
-    fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+    fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
       .then(res => res.json())
       .then(res => {
         setSearchResult(res.data)
@@ -38,7 +41,7 @@ function Search() {
         console.log(err)
         setLoading(false)
       })
-  }, [searchValue])
+  }, [debounced])
 
   // Handle
   const handleClearText = () => {
